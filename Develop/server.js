e7524fe9a476b97aeda647dd8fc52c2e9d6b8cf0
Goat <dpajbk@gmail.com>
2022-10-26 21:36:03 -0400

    index.html and notes.html pages have been added to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const path = require('path');
-
 const PORT = 3001
-
-const app =express()
+const app = express()
+const notes = require('./db/db.json')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
   });
-  
+
+app.get('/api/notes', (req, res) => res.json(notes));
+
+app.listen(PORT, () =>
+    console.log(`Serving static asset routes on port ${PORT}!`)
+);
